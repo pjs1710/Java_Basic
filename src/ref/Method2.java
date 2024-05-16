68fd1e10cfa4d89670e735edcb46e0690ebf841a
Park Jae Seong <timer973@naver.com>
2024-05-16 23:22:30 +0900

    기본형과 참조형 이론

--- src/ref/Method2.java
+++ src/ref/Method2.java
@@ -1,22 +1,21 @@
 package ref;
 
-public class Method1 {
+public class Method2 {
 
     public static void main(String[] args) {
-        Student student1 = new Student();
-        initStudent(student1, "학생1", 15, 90);
-
-        Student student2 = new Student();
-        initStudent(student2, "학생2", 16, 80);
+        Student student1 = createStudent("학생1", 15, 90);
+        Student student2 = createStudent("학생2", 16, 80);
 
         printStudent(student1);
         printStudent(student2);
     }
 
-    static void initStudent(Student student, String name, int age, int grade) {
+    static Student createStudent(String name, int age, int grade) {
+        Student student = new Student();
         student.name = name;
         student.age = age;
         student.grade = grade;
+        return student;
     }
 
     static void printStudent(Student student) {
