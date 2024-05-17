56b58082b395d6f8e15923c52a943597abd8fa07
Park Jae Seong <timer973@naver.com>
2024-05-17 15:18:29 +0900

    객체 지향 프로그래밍 이론

---
+++ src/oop1/MusicPlayer.java
@@ -0,0 +1,36 @@
+package oop1;
+
+public class MusicPlayer {
+ // 객체에서 속성과 기능을 어떻게 구현하는지가 중요하다
+    int volume = 0;
+    boolean isOn = false;
+
+    void on() {
+        isOn = true;
+        System.out.println("음악 플레이어를 시작합니다.");
+    }
+
+    void off() {
+        isOn = false;
+        System.out.println("음악 플레이어를 종료합니다.");
+    }
+
+    void volumeUp() {
+        volume++;
+        System.out.println("음악 플레이어 볼륨: " + volume);
+    }
+
+    void volumeDown() {
+        volume--;
+        System.out.println("음악 플레이어 볼륨: " + volume);
+    }
+
+    void showStatus() {
+        System.out.println("음악 플레이어 상태 확인");
+        if (isOn) {
+            System.out.println("음악 플레이어 On, 볼륨: " + volume);
+        } else {
+            System.out.println("음악 플레이어 Off");
+        }
+    }
+}
